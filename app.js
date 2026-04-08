1362b867c04f3862ed63c7f8105caf773292ebf7
housemouse62 <ryan.skeels@gmail.com>
2026-04-08 21:36:13 +0200

    prepare to deploy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", messagesRouter);
 app.use("/", authRouter);
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
     throw err;
   }
