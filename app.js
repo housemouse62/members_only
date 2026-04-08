737bed10e0ae90ba7ff39f6f49eb9150332314f0
housemouse62 <ryan.skeels@gmail.com>
2026-04-08 11:19:03 +0200

    add delete button for admin

--- app.js
+++ app.js
@@ -102,7 +102,6 @@ app.post(
     }
     try {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      console.log(req.body.admin);
       const { rows } = await pool.query(
         "INSERT INTO users (first_name, last_name, email, username, password, admin) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, username",
         [
@@ -159,9 +158,15 @@ app.post("/createMessage", async (req, res) => {
 
 app.get("/", async (req, res) => {
   const { rows } = await pool.query(
-    "SELECT users.username AS username, users.membership_status AS member, messages.title AS title, messages.text AS text, messages.timestamp AS timestamp FROM users INNER JOIN messages ON users.id=messages.user_id;",
+    "SELECT users.username AS username, users.membership_status AS member, messages.title AS title, messages.text AS text, messages.timestamp AS timestamp, messages.message_id AS message_id FROM users INNER JOIN messages ON users.id=messages.user_id;",
   );
   res.render("index", { user: req.user, messages: rows });
+});
+
+app.post("/", async (req, res) => {
+  const item = req.query.message_id;
+  await pool.query("DELETE FROM messages WHERE message_id = $1", [item]);
+  res.redirect("/");
 });
 
 app.listen(3000, (err) => {
