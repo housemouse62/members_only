0310b94502477add63d16caa9983eaec6cb52272
housemouse62 <ryan.skeels@gmail.com>
2026-04-08 11:32:06 +0200

    extract db pool and passport config into separate modules

--- app.js
+++ app.js
@@ -1,58 +1,10 @@
 const path = require("node:path");
-const { Pool } = require("pg");
+const pool = require("./db/pool");
 const express = require("express");
 const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs");
+const passport = require("./config/passport");
 const { body, matchedData, validationResult } = require("express-validator");
 
-const pool = new Pool({
-  host: "localhost",
-  user: "ryanskeels",
-  database: "members_only",
-  port: 5432,
-});
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const { rows } = await pool.query(
-        "SELECT * FROM users WHERE username = $1",
-        [username],
-      );
-      const user = rows[0];
-
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  }),
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
-    const user = rows[0];
-
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
